224e6c71a24a9b79c30d3b441ef5371f43f8be34
Evgeniya <osipova.dr17@yandex.ru>
2022-05-25 01:07:19 +1100

    Отправка электронной почты, коррекция ошибок

--- notify.php
+++ notify.php
@@ -8,32 +8,35 @@ use Symfony\Component\Mime\Email;
 
 require 'vendor/autoload.php';
 
-$dsn = 'smtp://water-lily77:qiwi2000FAKE@smtp.mail.ru:143';
+$dsn = 'smtp://water-lily77:gCJ1tzwqFb6T8vc2Wakb@smtp.mail.ru:465';
 $transport = Transport::fromDsn($dsn);
 
 $users = users_db($connect);
 
 foreach ($users as $user) {
-   
     $date = date('d-m-Y');
     $user_id = $user['id'];
     $tasks = get_deadline_tasks($connect, $user_id);
     $tasks_names = array_column($tasks, 'name');
-    if ($tasks === 0) {
+    $task_count = count($tasks_names);
+    if ($task_count === 0) {
         exit;
     } else {
-        $task_count = count($tasks_names);
         $all_tasks = implode(', ', $tasks_names);
         $plan = get_noun_plural_form($task_count, 'запланирована', 'запланированы', 'запланированы');
         $task = get_noun_plural_form($task_count, 'задача', 'задачи', 'задачи');
     
-        $massage = new Email();
-        $massage->to($user['email']);
-        $massage->from("keks@phpdemo.ru");
-        $massage->subject("Уведомление от сервиса «Дела в порядке»");
-        $message->text('Уважаемый(ая), ' . $user['name'] . '. У вас ' . $plan . $task . $all_tasks . ' на ' . $date . '.');
+        $message = new Email();
+        $message->to($user['email']);
+        $message->from("keks@phpdemo.ru");
+        $message->subject("Уведомление от сервиса «Дела в порядке»");
+        $message->text("Уважаемый(ая), ${user['name']}. У вас $plan $task $all_tasks на $date.");
     
         $mailer = new Mailer($transport);
-        $mailer->send($message);
+        if (!$mailer->send($message)) {
+            echo "Couldn't send email";
+        } else {
+            echo "Сообщение отправлено";
+        }
     }
 }
