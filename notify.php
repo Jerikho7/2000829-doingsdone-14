85e3f3d63fbc0ab9d42827fcc4cda51a0dc6ec31
Evgeniya <osipova.dr17@yandex.ru>
2022-05-25 20:21:49 +1100

    Отправка электронной почты

--- notify.php
+++ notify.php
@@ -12,20 +12,31 @@ $transport = Transport::fromDsn('smtp://dd8ecd6ba862b6:c67ceaa24b2e1a@smtp.mailt
 
 $mailer = new Mailer($transport);
 
-$users_tasks = get_deadline_tasks($connect);
+$users = users_db($connect);
 $date = date('d-m-Y');
-if (isset($users_tasks)) {
-    foreach ($users_tasks as $key => $value) {
+
+foreach ($users as $user) {
+    $user_id = $user['id'];
+    $tasks = get_deadline_tasks($connect, $user_id);
+    $tasks_names = array_column($tasks, 'name');
+    $task_count = count($tasks_names);
+    if (!empty($tasks)) {
+    
+        $all_tasks = implode(', ', $tasks_names);
+        $plan = get_noun_plural_form($task_count, 'запланирована', 'запланированы', 'запланированы');
+        $task = get_noun_plural_form($task_count, 'задача', 'задачи', 'задачи');
+    
         $message = new Email();
-        $message->to($value['email']);
+        $message->to($user['email']);
         $message->from("keks@phpdemo.ru");
         $message->subject("Уведомление от сервиса «Дела в порядке»");
-        $message->text("Уважаемый(ая), ${value['user_name']}. У вас запланирована задача ${value['task_name']} на $date.");
-    }
-    if ($mailer->send($message)) {
-        echo "Сообщение отправлено ${value['email']}";
-    } else {
-        echo "Сообщение не отправлено!";
+        $message->text("Уважаемый(ая), ${user['name']}. У вас $plan $task $all_tasks на $date.");
+    
+        $result = $mailer->send($message); 
+        if (!$result) {
+            echo "Сообщение не отправлено ${user['email']}! ";
+        } else {
+            echo "Сообщение отправлено ${user['email']}";
+        }
     }
 }
-
