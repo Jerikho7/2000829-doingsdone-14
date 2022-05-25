19b8fc7c45a6444e135b409e158e6d8c55ebf24a
Evgeniya <osipova.dr17@yandex.ru>
2022-05-25 21:07:59 +1100

    Добавление секции с кнопкой Добавить проект

--- templates/error_404.php
+++ templates/error_404.php
@@ -1,5 +1,15 @@
 <section class="content__side">
+    <h2 class="content__side-heading">Проекты</h2>
+
+    <nav class="main-navigation">
+        <ul class="main-navigation__list">
+        </ul>
+    </nav>
+
+    <a class="button button--transparent button--plus content__side-button"
+       href="add_project.php" target="project_add">Добавить проект</a>
 </section>
+
 <main class="content__main">
     <h2 class="content__header-text">Ошибка:</h2>
     <p><?= $error_404; ?></p>
