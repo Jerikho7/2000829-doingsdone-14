29c6ed00cd7a451703ecd99cdcbfeb5d5aaab49d
Evgeniya <osipova.dr17@yandex.ru>
2022-05-26 07:22:27 +1100

    Измененияя в конструкции if

--- notify.php
+++ notify.php
@@ -18,9 +18,9 @@ $date = date('d-m-Y');
 foreach ($users as $user) {
     $user_id = $user['id'];
     $tasks = get_deadline_tasks($connect, $user_id);
-    $tasks_names = array_column($tasks, 'name');
-   
+       
     if (!empty($tasks)) {
+        $tasks_names = array_column($tasks, 'name');
         $task_count = count($tasks_names);
         $all_tasks = implode(', ', $tasks_names);
         $plan = get_noun_plural_form($task_count, 'запланирована', 'запланированы', 'запланированы');
